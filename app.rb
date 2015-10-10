5e6dfa565d1d93cc4451b8ebed791562b6a3132b
Domenico <domeesposito@users.noreply.github.com>
2015-10-10 17:18:46 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,7 +4,6 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-
 get "/" do
   erb :home
 end
@@ -24,4 +23,9 @@ get "/create" do
   match = /(.*?)\.herokuapp\.com/.match(request.host)
   dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
   redirect to(dashboard_url)
+end
+
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
 end
